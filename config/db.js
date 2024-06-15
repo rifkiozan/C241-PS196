065180c1c3e16124ed3517bbf4267b43fd067b33
rayhanfth <154334687+rayhanfth@users.noreply.github.com>
2024-06-15 20:40:53 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,19 +1,5 @@
 const mysql = require('mysql2/promise');
 
-const connectDB = async () => {
-    try {
-        const connection = await mysql.createConnection({
-            host: '34.128.94.236',
-            user: 'root',
-            password: 'testdb',
-            database: 'testdb'
-        });
-        console.log('MySQL connected...');
-        return connection;
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
-    }
-};
+
 
 module.exports = connectDB;
