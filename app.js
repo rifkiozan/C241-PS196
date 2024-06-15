9311376b403635826029514c06d01a8c3aaa067e
rayhanfth <154334687+rayhanfth@users.noreply.github.com>
2024-06-15 22:24:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const resetPasswordRoutes = require('./routes/resetPasswordRoutes');
 const protectedRoutes = require('./routes/protectedRoutes');
+
 const app = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api', protectedRoutes);
 app.use('/api/reset-password', resetPasswordRoutes);
