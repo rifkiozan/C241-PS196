41297523b3ff5a03b1a02a36b4b2655db5739377
rayhanfth <154334687+rayhanfth@users.noreply.github.com>
2024-06-15 23:46:32 +0700

    Update emailService.js

--- services/emailService.js
+++ services/emailService.js
@@ -1,11 +1,7 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'huetifulproject@gmail.com',
-        pass: 'oenv rbpk extr jhbf'
-    }
+
 });
 
 const sendRegistrationEmail = async (to, name, verificationToken) => {
